bda3df9bbe4f34efcaa2759cd113a0b105371961
Davis Ford <daviseford@users.noreply.github.com>
2021-11-24 08:19:35 -0500

    Refactor RankingTable.tsx

--- app/src/components/RankingTable.tsx
+++ app/src/components/RankingTable.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from '@material-ui/core/Button'
 import ArrowForward from '@material-ui/icons/ArrowForward'
 import ArrowBack from '@material-ui/icons/ArrowBack'
@@ -7,156 +7,168 @@ import RollingImage from './RollingImage'
 
 const NUM_ITEMS_PER_PAGE = 10
 
-class RankingTable extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      page: 0,
-      numPages: 0,
-    }
-  }
+export interface RankingTableItem {
+  id: string
+  rank: number
+  nfts: string[]
+  name: string
+  rating: number
+  won: number
+  draw: number
+  lost: number
+}
 
-  componentDidMount = () => {
-    this.updatePagination()
-  }
+interface RankingTableProps {
+  data: RankingTableItem[]
+  runnerClicked: (item: RankingTableItem) => any
+  lastUpdateTimestamp: Date
+}
 
-  componentDidUpdate = prevProps => {
-    if (prevProps.data !== this.props.data) {
-      this.updatePagination()
-    }
-  }
+interface ButtonsProps {
+  page: number
+  numPages: number
+  back: () => void
+  next: () => void
+}
 
-  updatePagination = () => {
-    if (this.props.data) {
-      let n = this.props.data.length
-      let numPages = Math.ceil(n / NUM_ITEMS_PER_PAGE)
-      this.setState({
-        page: 0,
-        numPages: numPages,
-      })
-    } else {
-      this.setState({
-        page: 0,
-        numPages: 0,
-      })
-    }
-  }
+const Buttons: React.FC<ButtonsProps> = props => {
+  const { page, numPages, back, next } = props
 
-  back = () => {
-    if (this.state.page > 0) {
-      this.setState({ page: this.state.page - 1 })
-    }
-  }
-
-  next = () => {
-    if (this.state.page < this.state.numPages - 1) {
-      this.setState({ page: this.state.page + 1 })
-    }
-  }
-
-  renderItem(item) {
-    return (
-      <tr key={item.id} className="ranking-table-row" onClick={this.props.runnerClicked(item)}>
-        <td align="right" className="ranking-table-important">
-          #{item.rank}
-        </td>
-        <td align="center">
-          <RollingImage data={item.nfts} size={36} />
-        </td>
-        <td align="left">
-          <b>{item.name}</b>
-        </td>
-        <td align="center">{item.rating}</td>
-        <td align="center">{item.won}</td>
-        <td align="center">{item.draw}</td>
-        <td align="center">{item.lost}</td>
-      </tr>
-    )
-  }
-
-  renderItems(list) {
-    let ans = []
-    for (let i = 0; i < list.length; i++) {
-      ans.push(this.renderItem(list[i]))
-    }
-    return ans
-  }
-
-  renderList() {
-    if (this.props.data) {
-      let indexFrom = this.state.page * NUM_ITEMS_PER_PAGE
-      let indexTo = indexFrom + NUM_ITEMS_PER_PAGE
-      if (indexTo > this.props.data.length) {
-        indexTo = this.props.data.length
-      }
-      let list = this.props.data.slice(indexFrom, indexTo)
-      return (
-        <table style={{ width: '100%' }}>
-          <thead>
-            <tr style={{ lineHeight: '20px' }}>
-              <th align="right"> </th>
-              <th align="center"> </th>
-              <th align="left"> </th>
-              <th align="center">Rating</th>
-              <th align="center">Won</th>
-              <th align="center">Draw</th>
-              <th align="center">Lost</th>
-            </tr>
-          </thead>
-          <tbody>{this.renderItems(list)}</tbody>
-        </table>
-      )
-    } else {
-      return ''
-    }
-  }
-
-  renderButtons() {
-    if (this.props.data) {
-      return (
-        <div style={{ display: 'flex' }}>
-          <div>
-            <Button color="inherit" disabled={this.state.page === 0} onClick={this.back}>
-              <ArrowBack />
-            </Button>
-          </div>
-          <div style={{ textAlign: 'center' }}>
-            Page
-            <br />
-            {this.state.page + 1} / {this.state.numPages}
-          </div>
-          <div>
-            <Button
-              color="inherit"
-              disabled={this.state.page === this.state.numPages - 1}
-              onClick={this.next}
-            >
-              <ArrowForward />
-            </Button>
-          </div>
-        </div>
-      )
-    } else {
-      return ''
-    }
-  }
-
-  render() {
-    return (
-      <div className="ranking-panel">
-        <div className="panel-title">Ranking</div>
-        <div className="ranking-panel-table">{this.renderList()}</div>
-        <div className="ranking-panel-info">
-          Last updated:{' '}
-          <span className="my-green">
-            <strong>{this.props.lastUpdateTimestamp}</strong>
-          </span>
-          <br />
-          (Rating system uses ELO algorithm with the same parameters as MegaCity's Chess Organization)
-        </div>
-        <div className="ranking-panel-action">{this.renderButtons()}</div>
+  return (
+    <div style={{ display: 'flex' }}>
+      <div>
+        <Button color="inherit" disabled={page === 0} onClick={back}>
+          <ArrowBack />
+        </Button>
       </div>
-    )
+      <div style={{ textAlign: 'center' }}>
+        Page
+        <br />
+        {page + 1} / {numPages}
+      </div>
+      <div>
+        <Button color="inherit" disabled={page === numPages - 1} onClick={next}>
+          <ArrowForward />
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+interface ItemProps {
+  item: RankingTableItem
+  runnerClicked: RankingTableProps['runnerClicked']
+}
+
+const Item: React.FC<ItemProps> = props => {
+  const { item, runnerClicked } = props
+
+  return (
+    <tr key={item.id} className="ranking-table-row" onClick={runnerClicked(item)}>
+      <td align="right" className="ranking-table-important">
+        #{item.rank}
+      </td>
+      <td align="center">
+        <RollingImage data={item.nfts} size={36} />
+      </td>
+      <td align="left">
+        <b>{item.name}</b>
+      </td>
+      <td align="center">{item.rating}</td>
+      <td align="center">{item.won}</td>
+      <td align="center">{item.draw}</td>
+      <td align="center">{item.lost}</td>
+    </tr>
+  )
+}
+
+interface ItemListProps {
+  data?: RankingTableItem[]
+  runnerClicked: RankingTableProps['runnerClicked']
+  page: number
+}
+
+const ItemList: React.FC<ItemListProps> = props => {
+  const { data, page, runnerClicked } = props
+
+  if (!data) return null
+
+  const indexFrom = page * NUM_ITEMS_PER_PAGE
+
+  let indexTo = indexFrom + NUM_ITEMS_PER_PAGE
+  if (indexTo > data.length) {
+    indexTo = data.length
   }
+
+  const list = data.slice(indexFrom, indexTo)
+
+  return (
+    <table style={{ width: '100%' }}>
+      <thead>
+        <tr style={{ lineHeight: '20px' }}>
+          <th align="right"> </th>
+          <th align="center"> </th>
+          <th align="left"> </th>
+          <th align="center">Rating</th>
+          <th align="center">Won</th>
+          <th align="center">Draw</th>
+          <th align="center">Lost</th>
+        </tr>
+      </thead>
+      <tbody>
+        {list.map(x => (
+          <Item item={x} runnerClicked={runnerClicked} />
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
+const RankingTable: React.FC<RankingTableProps> = props => {
+  const [page, setPage] = useState(0)
+  const [numPages, setNumPages] = useState(0)
+
+  useEffect(() => {
+    if (props.data) {
+      const n = props.data.length
+      setNumPages(Math.ceil(n / NUM_ITEMS_PER_PAGE))
+    } else {
+      setNumPages(0)
+    }
+
+    setPage(0)
+  }, [props.data])
+
+  const back = () => {
+    if (page > 0) {
+      setPage(page - 1)
+    }
+  }
+
+  const next = () => {
+    if (page < numPages - 1) {
+      setPage(page + 1)
+    }
+  }
+
+  return (
+    <div className="ranking-panel">
+      <div className="panel-title">Ranking</div>
+      <div className="ranking-panel-table">
+        <ItemList data={props.data} runnerClicked={props.runnerClicked} page={page} />
+      </div>
+      <div className="ranking-panel-info">
+        Last updated:{' '}
+        <span className="my-green">
+          <strong>{props.lastUpdateTimestamp}</strong>
+        </span>
+        <br />
+        (Rating system uses ELO algorithm with the same parameters as MegaCity's Chess Organization)
+      </div>
+      <div className="ranking-panel-action">
+        {props.data && <Buttons numPages={numPages} page={page} back={back} next={next} />}
+      </div>
+    </div>
+  )
 }
 export default RankingTable
